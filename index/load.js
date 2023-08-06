b29fa69f56eba22efde48a7ce7d61b5b77a99582
arb dog <101073037+arbc2@users.noreply.github.com>
2023-08-06 12:36:01 -0700

    moved all to index
    etc

--- index/load.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-      $("#header").load("https://cabletwo.net/elements/header.html"); 
-      $("#fungame").load("https://cabletwo.net/elements/fungame.html"); 
-      $("#footer").load("https://cabletwo.net/elements/footer.html");
-      $("#overlay").load("https://cabletwo.net/live/videooverlay2.html");
-      $("#specialheader").load("https://cabletwo.net/elements/specialheader.html");
-      $("#memberpg").load("https://cabletwo.net/members/dtt2.html");
-
-});
---
+++ load.js
@@ -0,0 +1,9 @@
+$(function(){
+      $("#header").load("https://cabletwo.net/elements/header.html"); 
+      $("#fungame").load("https://cabletwo.net/elements/fungame.html"); 
+      $("#footer").load("https://cabletwo.net/elements/footer.html");
+      $("#overlay").load("https://cabletwo.net/live/videooverlay2.html");
+      $("#specialheader").load("https://cabletwo.net/elements/specialheader.html");
+      $("#memberpg").load("https://cabletwo.net/members/dtt2.html");
+
+});
