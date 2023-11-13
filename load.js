74a8acf2e1d68e18ea3c2990fa530d3dfbd332d8
arb dog <101073037+arbc2@users.noreply.github.com>
2023-11-12 16:14:34 -0800

    Update load.js

--- load.js
+++ load.js
@@ -1,11 +1,11 @@
 $(function(){
-      $("#header").load("https://cabletwo.net/elements/header.html"); 
+      $("#headerold").load("https://cabletwo.net/elements/header.html"); 
       $("#fungame").load("https://cabletwo.net/elements/fungame.html"); 
       $("#footer").load("https://cabletwo.net/elements/footer.html");
       $("#overlay").load("https://cabletwo.net/live/videooverlay2.html");
       $("#specialheader").load("https://cabletwo.net/elements/specialheader.html");
       $("#memberpg").load("https://cabletwo.net/elements/dtt2.html");
-      $("#smallhead").load("https://cabletwo.net/elements/way.html");
+      $("#header").load("https://cabletwo.net/elements/way.html");
       $("#carousel").load("https://cabletwo.net/elements/carousel.html");
 
 });
